e4de15a2cceb438dd39781e40df77d3609ca1b40
stefhanya <stefhanya.gonzalez@udea.edu.co>
2025-10-02 00:48:54 -0500

    transpuestas 'F' y 'C'

--- Stefhanya.py
+++ Stefhanya.py
@@ -1,7 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-v = np.random.rand(720)
-newv=v.reshape(120,6)
+v = np.random.rand(10)
+newv=v.reshape(5,2)
+vtrans = newv.T
+f = np.array(vtrans, order='F')  
+c = np.array(vtrans, order='C') 
+print('--------------vector v------------------')
 print(v)
+print('--------------nuevo v------------------')
 print(newv)
+print('--------------v transpuesta------------------')
+print(vtrans)
+print('--------------v transpuesta F------------------')
+print(f)
+print('--------------v transpuesta C------------------')
+print(v)
