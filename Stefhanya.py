7c47c3e9afae2f3aa0e2d47ea136c84cb162934b
stefhanya <stefhanya.gonzalez@udea.edu.co>
2025-10-04 15:11:35 -0500

    grafico diferente en cada panel

--- Stefhanya.py
+++ Stefhanya.py
@@ -23,4 +23,35 @@ ax3=plt.axes([0.12,0.95,0.3,0.2])
 ax4=plt.axes([1.35,0.1,0.8,0.4])
 ax5=plt.axes([0.5,0.95,0.5,0.3])
 ax6=plt.axes([1.33,0.7,0.6,0.5])
+# 1) Barras con error en ax1
+y = vtrans[0]
+x = np.arange(len(y))
+error = np.random.rand(len(y))
+ax1.bar(x, y, yerr=error, capsize=5, color="purple")
+ax1.set_xlim(0, 120)      
+ax1.set_ylim(-1, 2)       
+
+# 2) Histograma 2D en ax2
+ax2.hist2d(vtrans[1], vtrans[2], bins=6, cmap="plasma")
+ax2.set_xlim(-0.1 ,1.1)
+ax2.set_ylim(-0.1, 1.1)
+
+# 3) Boxplot en ax3
+ax3.boxplot(vtrans[3])
+ax3.set_xlim(0.5, 1.5)
+ax3.set_ylim(min(vtrans[3])-0.1, max(vtrans[3])+0.1)
+
+# 4) Histograma en ax4
+ax4.hist(vtrans[2], bins=8, color="pink", alpha=0.7)
+ax4.set_xlim(-0.1,1.1)
+ax4.set_ylim(-1, 26)
+
+# 5) Línea en ax5
+ax5.plot(vtrans[5], color="green")
+ax5.set_xlim(0, 120)
+ax5.set_ylim(0, 1)
+
+# 6) Pie chart en ax6
+ax6.pie(vtrans[3])
+
 plt.show()
