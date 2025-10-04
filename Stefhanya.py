c13523d65b80efb8b5580827c85322828c532b85
stefhanya <stefhanya.gonzalez@udea.edu.co>
2025-10-04 15:36:44 -0500

    titles y todo lo del fianl mas ordenado

--- Stefhanya.py
+++ Stefhanya.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 v = np.random.rand(720)
 newv=v.reshape(120,6)
 vtrans = newv.T
-f = np.array(vtrans, order='F')  
-c = np.array(vtrans, order='C') 
+f = np.array(vtrans, order='F')
+c = np.array(vtrans, order='C')
 print('--------------vector v------------------')
 print(v)
 print('--------------nuevo v------------------')
@@ -17,41 +17,59 @@ print(f)
 print('--------------v transpuesta C------------------')
 print(v)
 
-ax1 = plt.axes()
-ax2 = plt.axes([0.97, 0.35, 0.3, 0.5])
-ax3=plt.axes([0.12,0.95,0.3,0.2])
-ax4=plt.axes([1.35,0.1,0.8,0.4])
-ax5=plt.axes([0.5,0.95,0.5,0.3])
-ax6=plt.axes([1.33,0.7,0.6,0.5])
-# 1) Barras con error en ax1
+fig = plt.figure()  
+
+ax1 = fig.add_axes([0.0, 0.0, 0.9, 0.9])  
+ax2 = fig.add_axes([0.97, 0.35, 0.3, 0.5])
+ax3 = fig.add_axes([0.12, 1, 0.3, 0.2])
+ax4 = fig.add_axes([1.5, 0.1, 0.8, 0.4])
+ax5 = fig.add_axes([0.55, 1.02, 0.5, 0.3])
+ax6 = fig.add_axes([1.33, 0.7, 0.6, 0.5])
+
+# 1) Barras con error
 y = vtrans[0]
 x = np.arange(len(y))
 error = np.random.rand(len(y))
-ax1.bar(x, y, yerr=error, capsize=5, color="purple")
-ax1.set_xlim(0, 120)      
-ax1.set_ylim(-1, 2)       
+ax1.bar(x, y, yerr=error, capsize=5, color="purple", label="Valores con error")
+ax1.set_title("Barras con error")
+ax1.legend()
+ax1.grid(True)
 
-# 2) Histograma 2D en ax2
+# 2) Histograma 2D
 ax2.hist2d(vtrans[1], vtrans[2], bins=6, cmap="plasma")
-ax2.set_xlim(-0.1 ,1.1)
-ax2.set_ylim(-0.1, 1.1)
+ax2.set_title("Histograma 2D")
+ax2.grid(True)
 
-# 3) Boxplot en ax3
+# 3) Boxplot
 ax3.boxplot(vtrans[3])
-ax3.set_xlim(0.5, 1.5)
-ax3.set_ylim(min(vtrans[3])-0.1, max(vtrans[3])+0.1)
+ax3.set_title("Boxplot")
+ax3.grid(True)
 
-# 4) Histograma en ax4
-ax4.hist(vtrans[2], bins=8, color="pink", alpha=0.7)
-ax4.set_xlim(-0.1,1.1)
-ax4.set_ylim(-1, 26)
+# 4) Histograma
+ax4.hist(vtrans[2], bins=8, color="pink", alpha=0.7, label="Datos fila 2")
+ax4.set_title("Histograma")
+ax4.legend()
+ax4.grid(True)
 
-# 5) Línea en ax5
-ax5.plot(vtrans[5], color="green")
-ax5.set_xlim(0, 120)
-ax5.set_ylim(0, 1)
+# 5) Línea
+ax5.plot(vtrans[5], color="green", label="Fila 5")
+ax5.set_title("Gráfico de línea")
+ax5.legend()
+ax5.grid(True)
 
-# 6) Pie chart en ax6
-ax6.pie(vtrans[3])
+# 6) Pie chart
+fig = ax6.figure
+labels = [f"Dato {i}" for i in range(len(vtrans[2]))]
+wedges, texts = ax6.pie(vtrans[2])
+ax6.set_title("Gráfico de Pie")
+
+# leyenda solo del pie mostrando los datos
+fig.legend(wedges, labels,
+           loc="lower center",
+           bbox_to_anchor=(0.5, -0.2),
+           ncol=10,
+           fontsize=8,
+           title="Datos del Pie")
+plt.subplots_adjust(bottom=0.25)
 
 plt.show()
