81fe1e12bf87923963b4a58de249f553f135f2fc
stefhanya <stefhanya.gonzalez@udea.edu.co>
2025-10-04 15:09:01 -0500

    seis subplots a mano

--- Stefhanya.py
+++ Stefhanya.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-v = np.random.rand(10)
-newv=v.reshape(5,2)
+v = np.random.rand(720)
+newv=v.reshape(120,6)
 vtrans = newv.T
 f = np.array(vtrans, order='F')  
 c = np.array(vtrans, order='C') 
@@ -16,3 +16,11 @@ print('--------------v transpuesta F------------------')
 print(f)
 print('--------------v transpuesta C------------------')
 print(v)
+
+ax1 = plt.axes()
+ax2 = plt.axes([0.97, 0.35, 0.3, 0.5])
+ax3=plt.axes([0.12,0.95,0.3,0.2])
+ax4=plt.axes([1.35,0.1,0.8,0.4])
+ax5=plt.axes([0.5,0.95,0.5,0.3])
+ax6=plt.axes([1.33,0.7,0.6,0.5])
+plt.show()
